8b1d9c458fd24bbac87b562ef21194c04d0e45f5
Hansley2525 <Hansley2525@users.noreply.github.com>
2017-11-09 00:08:29 -0600

    Tried doing the carousel
    Just a preview

--- README.md
+++
@@ -1,7 +0,0 @@
-# COMP3020 Group 27
-
-COMP3020 - Human Computer Interaction I project
-
-Group members: Hansley Dawodharry, Min Gyu Park, Sabit Rahman
-
-Purpose: Create an ordering system for an imaginary burger restaurant named Ai. The goal is to make the system simple and interactive. 
--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-
 // Display correct page when button is clicked
 $('.sidebar-options').on( 'click', function() {
 	$('.page-content').hide();
 	$('#' + $(this).attr('id')+'-page').show();
+});
+
+
+// Instantiate the Bootstrap carousel
+$('.multi-item-carousel').carousel({
+  interval: false
+});
+
+// for every slide in carousel, copy the next slide's item in the slide.
+// Do the same for the next, next item.
+$('.multi-item-carousel .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+
+  if (next.next().length>0) {
+    next.next().children(':first-child').clone().appendTo($(this));
+  } else {
+  	$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+  }
 });
