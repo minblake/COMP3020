6fd512b25d2861cabfcdef5e974dd63d73c340f3
Min Gyu Park <parkmg@umanitoba.ca>
2017-11-10 07:39:00 -0600

    Implement add popular items to receipt
    selected popular item shown along with its quantity and picture. Need to work on removing and decreasing.

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-// Display correct page when button is clicked
-$('.sidebar-options').on( 'click', function() {
-	$('.page-content').hide();
-	$('#' + $(this).attr('id')+'-page').show();
-});
-
-// Display correct description when item is selected
-$('.item').on( 'click', function() {
-  $('.item').removeClass("selected");
-  $(this).addClass("selected");
-
-  $('.description-text').hide();
-  $('#' + 'description-' + $(this).attr('id')).show();
-});
 
 // Popular Items carousel
 $('#popular-carousel').owlCarousel({
@@ -29,5 +15,37 @@ $('#popular-carousel').owlCarousel({
   '<i class="fa fa-angle-right" aria-hidden="true"></i>' ] 
 });
 
+// Display correct page when button is clicked
+$('.sidebar-options').on( 'click', function() {
+  $('.page-content').hide();
+  $('#' + $(this).attr('id')+'-page').show();
+});
 
+// Display correct description when carousel item is selected
+$('.item').on( 'click', function() {
+  $('.item').removeClass("selected");
+  $(this).addClass("selected");
+
+  $('.description-text').hide();
+  $('#' + 'description-' + $(this).attr('id')).show();
+});
+
+// Add item to receipt list
+function addItem(obj) {
+  if (obj.length) {
+    var itemId = obj.attr('id');
+
+    // if already in the list, increase counter
+    if ( $('#' + itemId + '-receipt').length)
+      $('#' + itemId + '-quantity').html(function(i,val) { return +val+1});
+    
+    // else, add new item
+    else
+      $('#receipt-list').append('<li id="' + itemId + '-receipt"' + ' class="receipt-item">' +
+        '<img src="images/' + itemId + '-icon.png"> x<span id="' + itemId + '-quantity' +'">1</span></li>')
+  } 
+}
+
+// Add popular item burger
+$('#add-popular').on ( 'click', function() { addItem( $('.item.selected')); });
 
