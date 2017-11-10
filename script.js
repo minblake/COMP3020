8023a81191d6c094f2ca6f6d65a80c3b653171ad
Min Gyu Park <parkmg@umanitoba.ca>
2017-11-10 04:19:38 -0600

    Change implementation of carousel to Owl Carousel
    Change Bootstrap carousel to Owl carousel. Still need to add price.
    Implement showing description of selected popular item

--- script.js
+++ script.js
@@ -4,4 +4,30 @@ $('.sidebar-options').on( 'click', function() {
 	$('#' + $(this).attr('id')+'-page').show();
 });
 
+// Display correct description when item is selected
+$('.item').on( 'click', function() {
+  $('.item').removeClass("selected");
+  $(this).addClass("selected");
+
+  $('.description-text').hide();
+  $('#' + 'description-' + $(this).attr('id')).show();
+});
+
+// Popular Items carousel
+$('#popular-carousel').owlCarousel({
+  mouseDrag: false,
+  touchDrag: false,
+  pullDrag: false,
+
+  margin: 10,
+  nav: true,
+  autoHeight: true,
+  items: 6,
+
+  //Define navigation icons as < >
+  navText : [ '<i class="fa fa-angle-left" aria-hidden="true"></i>', 
+  '<i class="fa fa-angle-right" aria-hidden="true"></i>' ] 
+});
+
+
 
