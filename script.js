8b70d3c9a6cc59714fc9632ee4277570e34fa83a
Min Gyu Park <parkmg@umanitoba.ca>
2017-11-15 00:46:32 -0600

    Responsive layout
    Change to Bootstrap 3.3.7
    Toggle button for receipt and sidebar
    Still when resizing, there are certain bugs

--- script.js
+++ script.js
@@ -1,51 +1,82 @@
+$(document).ready(function () {
+    $('#sidebar-toggle').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
 
-// Popular Items carousel
-$('#popular-carousel').owlCarousel({
-  mouseDrag: false,
-  touchDrag: false,
-  pullDrag: false,
+    $('#receipt-toggle').on('click', function () {
+        $('#receipt-bar').toggleClass('active');
+    });
 
-  margin: 10,
-  nav: true,
-  autoHeight: true,
-  items: 6,
+    //Home button pressed from start
+    $('#home-option').trigger('click');
 
-  //Define navigation icons as < >
-  navText : [ '<i class="fa fa-angle-left" aria-hidden="true"></i>', 
-  '<i class="fa fa-angle-right" aria-hidden="true"></i>' ] 
 });
 
 // Display correct page when button is clicked
-$('.sidebar-options').on( 'click', function() {
-  $('.page-content').hide();
-  $('#' + $(this).attr('id')+'-page').show();
+$('.sidebar-options').on('click', function () {
+    $('.page-content').hide();
+    $('#' + $(this).attr('id') + '-page').show();
+
+    $('.sidebar-options').removeClass('link-hover');
+    $(this).addClass('link-hover');
+
 });
 
-// Display correct description when carousel item is selected
-$('.item').on( 'click', function() {
-  $('.item').removeClass("selected");
-  $(this).addClass("selected");
+// Popular Items carousel
+$('#popular-carousel').owlCarousel({
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    margin: 10,
+    nav: true,
+    autoHeight: true,
+    // items: 5,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        992: {
+            items: 6
+        }
+    },
+    //Define navigation icons as < >
+    navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        '<i class="fa fa-angle-right" aria-hidden="true"></i>']
+});
 
-  $('.description-text').hide();
-  $('#' + 'description-' + $(this).attr('id')).show();
+
+// Display correct description when carousel item is selected
+$('.item').on('click', function () {
+    $('.item').removeClass('item-hover');
+    $(this).addClass('item-hover');
+
+    $('.description-text').hide();
+    $('#' + 'description-' + $(this).attr('id')).show();
+
 });
 
 // Add item to receipt list
 function addItem(obj) {
-  if (obj.length) {
-    var itemId = obj.attr('id');
+    if (obj.length) {
+        var itemId = obj.attr('id');
 
-    // if already in the list, increase counter
-    if ( $('#' + itemId + '-receipt').length)
-      $('#' + itemId + '-quantity').html(function(i,val) { return +val+1});
-    
-    // else, add new item
-    else
-      $('#receipt-list').append('<li id="' + itemId + '-receipt"' + ' class="receipt-item">' +
-        '<img src="images/' + itemId + '-icon.png"> x<span id="' + itemId + '-quantity' +'">1</span></li>')
-  } 
+        // if already in the list, increase counter
+        if ($('#' + itemId + '-receipt').length)
+            $('#' + itemId + '-quantity').html(function (i, val) {
+                return +val + 1
+            });
+
+        // else, add new item
+        else
+            $('#receipt-list').append('<li id="' + itemId + '-receipt"' + ' class="receipt-item">' +
+                '<img src="images/' + itemId + '-icon.png"> x<span id="' + itemId + '-quantity' + '">1</span></li>');
+    }
 }
 
 // Add popular item burger
-$('#add-popular').on ( 'click', function() { addItem( $('.item.selected')); });
-
+$('#add-popular').on('click', function () {
+    addItem($('.item.item-hover'));
+});
